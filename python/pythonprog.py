fa8a5b5078596f91f144000e0ffbb35e3c731b0e
yogarajanai01 <yogarajanai01@gmail.com>
2024-02-09 10:26:35 +0530

    program modified

--- python/pythonprog.py
+++ python/pythonprog.py
@@ -1,31 +1,31 @@
 class Student:
-	def __init__(self):
-		self.student_details = {}
+	def __init__(bright):
+		bright.student_details = {}
  
 	@property
-	def input_student_details(self):	
+	def input_student_details(bright):	
 		roll_number = input("Enter Roll Number : ")
 		name = input("Enter Student Name : ")
-		marks = float(input("Enter Marks : "))
-		self.student_details[roll_number] = {'name': name, 'marks': marks}
-		print("Marks Added successfully")
+		Attendancepercent = int(input("Enter Attendance percentage : "))
+		bright.student_details[roll_number] = {'name': name, 'Attendance percentage': Attendancepercent}
+		print("Attendance percentage Added successfully")
  
-	def update_student_marks(self):	 
-		roll_number = input("Enter Roll Number of the Student to Update Marks : ")
-		if roll_number in self.student_details:
-			new_marks = float(input("Enter New Marks : "))
-			self.student_details[roll_number]['marks'] = new_marks
-			print("Marks updated successfully")
+	def update_student_Attendancepercent(bright):	 
+		roll_number = input("Enter Roll Number of the Student to Update Attendance percentage : ")
+		if roll_number in bright.student_details:
+			new_Attendancepercent = int(input("Enter New Attendance percentage : "))
+			bright.student_details[roll_number]['Attendance percentage'] = new_Attendancepercent
+			print("Attendance percentage updated successfully")
 		else:
 			print("Student not found")
  
-	def print_student_details(self):	
+	def print_student_details(bright):	
 		roll_number = input("Enter Roll Number of the student to view details : ")
-		if roll_number in self.student_details:
-			student = self.student_details[roll_number]
+		if roll_number in bright.student_details:
+			student = bright.student_details[roll_number]
 			print("Roll Number :", roll_number)
 			print("Student Name :", student['name'])
-			print("Marks :", student['marks'])
+			print("Attendance percentage :", student['Attendance percentage'])
 		else:
 			print("Student not found")
  
@@ -34,7 +34,7 @@ obj = Student()
 while True:
     print("\n ********* Student Management System *********  ")
     print("\n1. Input Student Details")
-    print("2. Update Student Marks")
+    print("2. Update Student Attendance percentage")
     print("3. Print Student Details")
     print("4. Exit")
  
@@ -43,7 +43,7 @@ while True:
     if choice == '1':
         obj.input_student_details
     elif choice == '2':
-        obj.update_student_marks()
+        obj.update_student_Attendancepercent()
     elif choice == '3':
         obj.print_student_details()
     elif choice == '4':
