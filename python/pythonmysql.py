2ecf7808ce38acb32aa38ceb102220f0b6486f9c
yogarajanai01 <yogarajanai01@gmail.com>
2024-02-19 13:36:21 +0530

    nothing modified only visited

--- python/pythonmysql.py
+++ python/pythonmysql.py
@@ -3,11 +3,16 @@ con = mysql.connector.connect(
     host="192.168.1.240",
     user="AIBATCH01",
     password="AI@123",
-    database="a"
+    database="ai_yogarajan"
 )
 print(con)
 result=con.cursor()
-result.execute("show tables")
-resultshow=result.fetchall()
-for x in resultshow:
-    print(x)
+#result.execute("show tables")
+#resultshow=result.fetchall()
+#for x in resultshow:
+ #   print(x)
+#result.execute("select * from kabaddi_players")
+#resultshow=result.fetchall()
+#for x in resultshow:
+ #   print(x)    
+ 
