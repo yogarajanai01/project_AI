d6dd8c71fb13ede79635c1545f194adf05989e02
yogarajanai01 <yogarajanai01@gmail.com>
2024-02-22 11:45:10 +0530

    notepad demo page made

---
+++ python/dbdemo/menubar.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+
+menu=tk.Tk()
+menu.geometry("500x500")
+menu.title("NOTEPAD.007")
+
+def quit():
+    menu.destroy()
+
+def aboutpage():
+    abt=tk.Tk()
+    abt.title("About us")
+    abt.geometry("300x300")
+    message="""Welcome to NOTEPAD.007
+    created on : 22-02-2024
+    by Yoga The AITECH.
+    """
+    lntinfo=tk.Label(abt,text=message)
+    lntinfo.pack()
+    abt.mainloop()    
+
+def New():
+    menu=tk.Tk()
+    menu.geometry("500x500")
+    menu.title("untitled.1")
+
+def New_Window():
+    menu=tk.Tk()
+    menu.geometry("500x500")
+    menu.title("NOTEPAD.007")
+    menubar=tk.Menu(menu)
+    filemenu=tk.Menu(menubar,tearoff=0)
+    menubar.add_cascade(label="File",menu=filemenu,underline=0)
+    filemenu.add_command(label="New     Ctrl+N",underline=0,command=New)
+    filemenu.add_command(label="New Window      Ctrl+W",underline=4,command=New_Window)
+    filemenu.add_command(label="Open        Ctrl+O",underline=0)
+    filemenu.add_command(label="Save        Ctrl+S",underline=0)
+    filemenu.add_command(label="Save As     Ctrl+A",underline=5)
+    filemenu.add_separator()
+    filemenu.add_command(label="Page Setup      Ctrl+P",underline=0)
+    filemenu.add_command(label="Print       Ctrl+r",underline=1)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit        Ctrl+E",underline=0,command=quit)
+
+    editmenu=tk.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Edit",menu=editmenu)
+    editmenu.add_command(label="Undo",  underline=0, accelerator="Alt+F")
+    editmenu.add_command(label="Redo",  underline=4, accelerator="Alt+F")
+    editmenu.add_separator()
+    editmenu.add_command(label="Copy",  underline=0, accelerator="Alt+F")
+    editmenu.add_command(label="Cut",  underline=0, accelerator="Alt+F")
+    editmenu.add_command(label="Paste",  underline=0, accelerator="Alt+F")
+    
+    viewmenu=tk.Menu(menubar)
+    menubar.add_cascade(label="View")
+
+    helpmenu=tk.Menu(menubar,tearoff=0)
+    menubar.add_cascade(label="Help",menu=helpmenu)
+    helpmenu.add_command(label="About",  underline=0, accelerator="Alt+F", command=aboutpage)
+    menu.config(menu=menubar)
+
+    
+                                                                           
+
+menubar=tk.Menu(menu)
+filemenu=tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="File",menu=filemenu,underline=0)
+filemenu.add_command(label="New     Ctrl+N",underline=0,command=New)
+filemenu.add_command(label="New Window      Ctrl+W",underline=4,command=New_Window)
+filemenu.add_command(label="Open        Ctrl+O",underline=0)
+filemenu.add_command(label="Save        Ctrl+S",underline=0)
+filemenu.add_command(label="Save As     Ctrl+A",underline=5)
+filemenu.add_separator()
+filemenu.add_command(label="Page Setup      Ctrl+P",underline=0)
+filemenu.add_command(label="Print       Ctrl+r",underline=1)
+filemenu.add_separator()
+filemenu.add_command(label="Exit        Ctrl+E",underline=0,command=quit)
+
+editmenu=tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Edit",menu=editmenu)
+editmenu.add_command(label="Undo",  underline=0, accelerator="Alt+F")
+editmenu.add_command(label="Redo",  underline=4, accelerator="Alt+F")
+editmenu.add_separator()
+editmenu.add_command(label="Copy",  underline=0, accelerator="Alt+F")
+editmenu.add_command(label="Cut",  underline=0, accelerator="Alt+F")
+editmenu.add_command(label="Paste",  underline=0, accelerator="Alt+F")
+
+viewmenu=tk.Menu(menubar)
+menubar.add_cascade(label="View")
+
+helpmenu=tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Help",menu=helpmenu)
+helpmenu.add_command(label="About",  underline=0, accelerator="Alt+F", command=aboutpage)
+menu.config(menu=menubar)
+
+menu.mainloop()
