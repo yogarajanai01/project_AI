79897d7648c4d95285d6a6399505201499bfedd4
yogarajanai01 <yogarajanai01@gmail.com>
2024-02-22 10:04:37 +0530

    sql connectivity and manipulations are done using pyhton

--- python/databaseoperation.py
+++ python/databaseoperation.py
@@ -126,99 +126,86 @@ def delete():
     e_con.commit()
 
     outmsg.config(text="deleted")
-def reset():
-    pass
-def truncate():
-    pass
-
 
 labelname=Label(data,text="NAME")
-labelname.grid(row=0,column=50,padx=20,pady=20)
+labelname.grid(row=0,column=100,padx=20,pady=20)
 labelname.config(bg="blue",fg="white")
 
 inputName=Entry(data)
-inputName.grid(row=0,column=100)
+inputName.grid(row=0,column=200)
 inputName.config(bg="blue",fg="white")
 
 labelroll=Label(data,text="ROLL_NO")
-labelroll.grid(row=2,column=50,padx=20,pady=20)
+labelroll.grid(row=2,column=100,padx=20,pady=20)
 labelroll.config(bg="blue",fg="white")
 
 inputroll=Entry(data)
-inputroll.grid(row=2,column=100)
+inputroll.grid(row=2,column=200)
 inputroll.config(bg="blue",fg="white")
 
 TAM=Label(data,text="TAMIL")
-TAM.grid(row=10,column=50,padx=20,pady=20)
+TAM.grid(row=10,column=100,padx=20,pady=20)
 TAM.config(bg="blue",fg="white")
 
 inputt=Entry(data)
-inputt.grid(row=10,column=100)
+inputt.grid(row=10,column=200)
 inputt.config(bg="blue",fg="white")
 
 ENG=Label(data,text="ENGLISH")
-ENG.grid(row=12,column=50,padx=20,pady=20)
+ENG.grid(row=12,column=100,padx=20,pady=20)
 ENG.config(bg="blue",fg="white")
 
 inputE=Entry(data)
-inputE.grid(row=12,column=100)
+inputE.grid(row=12,column=200)
 inputE.config(bg="blue",fg="white")
 
 MATH=Label(data,text="MATHS")
-MATH.grid(row=14,column=50,padx=20,pady=20)
+MATH.grid(row=14,column=100,padx=20,pady=20)
 MATH.config(bg="blue",fg="white")
 
 inputM=Entry(data)
-inputM.grid(row=14,column=100)
+inputM.grid(row=14,column=200)
 inputM.config(bg="blue",fg="white")
 
 PHY=Label(data,text="PHYSICS")
-PHY.grid(row=16,column=50,padx=20,pady=20)
+PHY.grid(row=16,column=100,padx=20,pady=20)
 PHY.config(bg="blue",fg="white")
 
 inputP=Entry(data)
-inputP.grid(row=16,column=100)
+inputP.grid(row=16,column=200)
 inputP.config(bg="blue",fg="white")
 
 insbut=Button(data,text="INSERT",command=insert)
-insbut.grid(row=25,column=20,padx=40,pady=40)
+insbut.grid(row=25,column=100,padx=40,pady=40)
 insbut.config(bg="black",fg="white")
 
 insbut=Button(data,text="UPDATENAME",command=updatename)
-insbut.grid(row=25,column=50,padx=40,pady=10)
+insbut.grid(row=25,column=200,padx=40,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="UPDATEROLLNO",command=updateroll)
-insbut.grid(row=26,column=50,pady=10)
+insbut.grid(row=26,column=200,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="UPDATETAMIL",command=updateTAMIL)
-insbut.grid(row=27,column=50,pady=10)
+insbut.grid(row=27,column=200,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="UPDATEENGLISH",command=updateEng)
-insbut.grid(row=28,column=50,pady=10)
+insbut.grid(row=28,column=200,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="UPDATEMATHS",command=updateMath)
-insbut.grid(row=29,column=50,pady=10)
+insbut.grid(row=29,column=200,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="UPDATEPHYSICS",command=updatePhy)
-insbut.grid(row=30,column=50,pady=10)
+insbut.grid(row=30,column=200,pady=10)
 insbut.config(bg="green",fg="yellow")
 
 insbut=Button(data,text="DELETE",command=delete)
-insbut.grid(row=25,column=80,padx=40,pady=40)
+insbut.grid(row=25,column=800,padx=40,pady=40)
 insbut.config(bg="red")
-
-insbut=Button(data,text="RESET")
-insbut.grid(row=25,column=150,padx=40,pady=40)
-insbut.config(bg="orange")
-
-insbut=Button(data,text="TRUNCATE")
-insbut.grid(row=25,column=250,padx=40,pady=40)
-insbut.config(bg="dark red")
 
 lblsno=Label(data,text="SNO",bg="white",fg="black")
 lblsno.grid(row=0,column=400)
@@ -226,8 +213,8 @@ lblsno.grid(row=0,column=400)
 inputsno=Entry(data,text="",bg="white",fg="black")
 inputsno.grid(row=0,column=550)
 
-outmsg=Label(data,text="output",bg="white",fg="black")
-outmsg.grid(row=2,column=400)
+outmsg=Label(data,text="",bg="black",fg="white")
+outmsg.grid(row=2,column=500)
 
 data.mainloop()
 
